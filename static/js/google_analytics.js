879933aaad88203aa5e2c97d00b8916265c493ff
Yaroslav Petryshyn <ya6liszl@gmail.com>
2024-07-11 14:13:56 +0300

    Fix cookies

---
+++ static/js/google_analytics.js
@@ -0,0 +1,25 @@
+(function() {
+    var respectDoNotTrack = {{ .Site.Config.Privacy.GoogleAnalytics.RespectDoNotTrack }};
+    var gaID = "{{ .Site.Config.Services.GoogleAnalytics.ID }}";
+
+    if (respectDoNotTrack) {
+        var dnt = (navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack);
+        var doNotTrack = (dnt == "1" || dnt == "yes");
+    }
+
+    if (!doNotTrack) {
+        var gaScript = document.createElement('script');
+        gaScript.src = "https://www.googletagmanager.com/gtag/js?id=" + gaID;
+        gaScript.async = true;
+        document.head.appendChild(gaScript);
+
+        gaScript.onload = function () {
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', gaID);
+        };
+    } else {
+        console.warn("Do Not Track is enabled, Google Analytics will not load.");
+    }
+})();
