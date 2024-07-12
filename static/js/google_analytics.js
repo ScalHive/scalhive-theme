f7249ec346df09f08d31848b4b3b4221991e1932
Yaroslav Petryshyn <ya6liszl@gmail.com>
2024-07-12 03:25:32 +0300

    Fix base cookies

--- static/js/google_analytics.js
+++ static/js/google_analytics.js
@@ -1,28 +1,29 @@
 (function() {
-    var doNotTrack = localStorage.getItem("doNotTrack");
-    //
-    // var respectDoNotTrack = {{ .Site.Config.Privacy.GoogleAnalytics.RespectDoNotTrack }};
-    // var gaID = "{{ .Site.Config.Services.GoogleAnalytics.ID }}";
-    //
-    // if (respectDoNotTrack) {
-    //     var dnt = (navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack);
-    //     doNotTrack = (dnt == "1" || dnt == "yes");
-    // }
-    //
-    // if (!doNotTrack) {
-    //     var gaScript = document.createElement('script');
-    //     gaScript.src = "https://www.googletagmanager.com/gtag/js?id=" + gaID;
-    //     document.head.appendChild(gaScript);
-    //
-    //     gaScript.onload = function () {
-    //         window.dataLayer = window.dataLayer || [];
-    //         function gtag(){dataLayer.push(arguments);}
-    //         gtag('js', new Date());
-    //         gtag('config', gaID);
-    //     };
-    // } else {
-    //     console.warn("Do Not Track is enabled, Google Analytics will not load.");
-    // }
-    console.log("------------------")
-    console.log(doNotTrack)
+    var doNotTrack = false;
+    var localStorageDNT = localStorage.getItem("doNotTrack");
+
+    if (localStorageDNT === "1") {
+        doNotTrack = true;
+    } else if (localStorageDNT === "0") {
+        doNotTrack = false;
+    }
+
+    if (respectDoNotTrack) {
+        var dnt = (navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack);
+        doNotTrack = (dnt == "1" || dnt == "yes");
+    }
+
+    if (!doNotTrack) {
+        var gaScript = document.createElement('script');
+        gaScript.src = "https://www.googletagmanager.com/gtag/js?id=" + googleAnalyticsID;
+        gaScript.async = true;
+        document.head.appendChild(gaScript);
+
+        gaScript.onload = function () {
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', googleAnalyticsID);
+        };
+    }
 })();
